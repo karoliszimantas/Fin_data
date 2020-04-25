aa8c0d3b7cd0125ea15643e065439c81dd55ebea
karoliszimantas <zimantask@gmail.com>
2020-04-25 21:28:29 +0300

    new function income statement analysis

--- main_body.py
+++ main_body.py
@@ -34,21 +34,21 @@ def plot_plots(years, ylabel, attribute):
     plt.xticks(x_axis, x_axis)
     plt.show()
 
-def plot_income(years):
+def plot_balance(years):
     fig, ax1 = plt.subplots()
     width = 0.22
     x_axis = np.arange(1, len(years) + 1)
-    rev_labels = [int(getattr(year, 'rev',)) for year in years]
-    net_labels = [int(getattr(year, 'net',)) for year in years]
-    ax1.bar(x_axis - width/2 ,rev_labels, width, label='Revenue')
-    ax1.bar(x_axis + width/2, net_labels, width, label='Net income')
+    t_asset_labels = [int(getattr(year, 'assets')) for year in years]
+    t_liabl_labels = [int(getattr(year, 'liab')) for year in years]
+    ax1.bar(x_axis - width/2 ,t_asset_labels, width, label='Total assets')
+    ax1.bar(x_axis + width/2, t_liabl_labels, width, label='Total liabilities')
     ax1.set_xlabel('Periods')
     ax1.legend()
 
     ax2 = ax1.twinx()
 
-    profit_margin = [a / b * 100 for a, b in zip(net_labels, rev_labels)]
-    ax2.plot(x_axis,profit_margin, color='red', label='Profit margin', lw=3)
+    debt_assets = [a / b * 100 for a, b in zip(t_liabl_labels, t_asset_labels)]
+    ax2.plot(x_axis,debt_assets, color='red', label='Debt to Assets', lw=3)
     ax2.set_ylabel('%')
     ax2.legend(loc=2)
 
@@ -56,6 +56,27 @@ def plot_income(years):
     fig.tight_layout()
     plt.show()
 
+def plot_income(years):
+    fig, ax1 = plt.subplots()
+    width = 0.22
+    x_axis = np.arange(1, len(years) + 1)
+    rev_labels = [int(getattr(year, 'rev', )) for year in years]
+    net_labels = [int(getattr(year, 'net', )) for year in years]
+    ax1.bar(x_axis - width / 2, rev_labels, width, label='Revenue')
+    ax1.bar(x_axis + width / 2, net_labels, width, label='Net income')
+    ax1.set_xlabel('Periods')
+    ax1.legend()
+
+    ax2 = ax1.twinx()
+
+    profit_margin = [a / b * 100 for a, b in zip(net_labels, rev_labels)]
+    ax2.plot(x_axis, profit_margin, color='red', label='Profit margin', lw=3)
+    ax2.set_ylabel('%')
+    ax2.legend(loc=2)
+
+    plt.xticks(x_axis, x_axis)
+    fig.tight_layout()
+    plt.show()
 
     '''for indexes,heights in enumerate(labels[1:],1):
         gud_index = str(indexes)
@@ -142,8 +163,8 @@ def main_loop():
         print("Welcome to financial analysis. This project will help you to visualise financial data. "
               "You can choose to compare historical data over a number of accounting periods (horizontal analysis) "
               "or analyse single reporting period (vertical analysis)")
-        select_menu = int(input("Please enter 1 (vertical analysis) or 2 (horizontal analysis) or income 3 : "))
-        if select_menu in [1, 2, 3 ]:
+        select_menu = int(input("Please enter 1 (vertical analysis) \n2 (horizontal analysis) \n3 income statement\n4 balance sheet "))
+        if select_menu in [1, 2, 3, 4]:
             if select_menu == 1:
                 # under development. Target: to identify delta between values and diferiancate color of text according to output
                 # class Features:
@@ -170,6 +191,18 @@ def main_loop():
                         year = Year(*attrs)
                         years.append(year)
                     plot_income(years)
+                    end = input('Enter "Q" if you wat to go back.\n Enter any key to re enter periods')
+                    if end in ('Q', 'q'):
+                        sys.exit()
+            elif select_menu == 4:
+                while True:
+                    select_h_main = int(input("Enter the number of comparative periods\n 2 year\n 3 year\n 4 year: "))
+                    years = []
+                    for year_id in range(select_h_main):
+                        attrs = input_attrs('Enter ' + str(year_id + 1) + ' period data: ')
+                        year = Year(*attrs)
+                        years.append(year)
+                    plot_balance(years)
             else:
                 print("Wrong choice")
         end = input('Enter "Q" if you want to exit.\n Enter any key to select other option: ')
